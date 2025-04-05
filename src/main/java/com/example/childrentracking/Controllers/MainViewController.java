92f7fe41d9a72e6a68946ebe61d8cb1ce3f2d504
nguyenminhquang <nnguyenminhquang786@gmail.com>
2025-04-06 00:07:57 +0700

    small updates

---
+++ src/main/java/com/example/childrentracking/Controllers/MainViewController.java
@@ -0,0 +1,36 @@
+package com.example.childrentracking.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainViewController {
+    @FXML
+    private void handleHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleReport(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleSettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleSchedule(ActionEvent event) {
+
+    }
+}
